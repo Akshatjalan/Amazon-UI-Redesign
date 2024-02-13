127732c39eaf328c0065564c685e5eb105d28cdd
Akshat Jalan <Jalanakshat2@gmail.com>
2024-02-14 00:37:36 +0530

    added loader spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import loader from "./assets/loaderSpinner.gif";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3200);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loader">
+        <img src={loader} alt="loading" />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <div>
